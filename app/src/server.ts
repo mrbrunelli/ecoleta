a44ff38b5b3004b1e883bb04cd4170c37fc10c76
Matheus Ricardo Brunelli <matheus.brunelli@gmail.com>
2020-06-02 18:22:12 -0300

    Aula 2, Foi adicionado um arquivo para anotações. Foi adicionado uma pasta controller para organizar o código. Foi adiconado uma pasta uploads para subir todas as fotos para lá. Foi adicionado o arquivo de conexão com o banco. Foi utilizado o query params para criar filtros na rota de points.

--- app/src/server.ts
+++ app/src/server.ts
@@ -1,45 +1,14 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(routes)
 
-const users = [
-    'Matheus', // 0
-    'Vinicius', // 1
-    'Minhoca', // 2
-    'Bruno',
-    'Lua',
-    'Estrelinha'
-]
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search)
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
-
-    return response.json(filteredUsers)
-})
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    const user = users[id]
-
-    return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-    const data = request.body
-
-    console.log(data)
-
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-
-    return response.json(user)
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
